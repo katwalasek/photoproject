036329cf38a06586e46686b8da64861cd99aa80d
katwalasek <k.m.walasek@gmail.com>
2020-04-15 20:55:04 +0200

    update

--- js/app.js
+++ js/app.js
@@ -5,9 +5,28 @@ const menuTriggerEl = document.querySelector(".menu-trigger-el");
         nestedMenu.style.display = "block";
     });
     
-    
     nestedMenu.addEventListener("mouseout", function() {
         this.style.display = "none";
         
     });  
+    
+    const readMoreLessButton = document.querySelector(".read-more-less-button");
+    const additionalText = document.querySelector(".additional-paragraph");
+    
+    
+    readMoreLessButton.addEventListener("click", function() {
+      if (additionalText.style.display === "none" || additionalText.style.display === "") {
+          additionalText.style.display = "block";
+          readMoreLessButton.textContent = "Read less";
+      } else { 
+          additionalText.style.display = "none";
+          readMoreLessButton.textContent = "Read more";
+           }
+         });
+    
 });
+
+
+
+
+
