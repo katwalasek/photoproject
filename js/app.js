b7d99082f6904d955fe44354e420d3a5a46f0ef9
katwalasek <k.m.walasek@gmail.com>
2020-04-18 20:17:41 +0200

    update

--- js/app.js
+++ js/app.js
@@ -10,19 +10,45 @@ const menuTriggerEl = document.querySelector(".menu-trigger-el");
         
     });  
     
-    const readMoreLessButton = document.querySelector(".read-more-less-button");
-    const additionalText = document.querySelector(".additional-paragraph");
-    
-    
-    readMoreLessButton.addEventListener("click", function() {
-      if (additionalText.style.display === "none" || additionalText.style.display === "") {
-          additionalText.style.display = "block";
-          readMoreLessButton.textContent = "Read less";
+    const readMoreLessButtons = document.querySelectorAll(".read-more-less-button");
+     
+    function showHideText() {
+        const siblingText = this.previousElementSibling;
+        
+      if (siblingText.style.display === "none" || siblingText.style.display === "") {
+          siblingText.style.display = "block";
+          this.textContent = "Read less";
       } else { 
-          additionalText.style.display = "none";
-          readMoreLessButton.textContent = "Read more";
-           }
-         });
+          siblingText.style.display = "none";
+          this.textContent = "Read more";
+           }  
+    }
+    
+   for (let i = 0; i < readMoreLessButtons.length; i++) {
+       readMoreLessButtons[i].addEventListener("click", showHideText);
+   }
+   
+    
+    const input = document.querySelector(".newsletter-form input");
+    const newsletterButton = document.querySelector(".newsletter-form button");
+    const formInfo = document.querySelector(".form-info");
+    console.log(formInfo);
+    
+    newsletterButton.addEventListener("click", function(event) {
+        event.preventDefault();
+        formInfo.style.display = "block";
+        
+        if (input.value !=="") {
+            formInfo.textContent = "You have been subscribed";
+            input.value = "";
+        } else {
+            formInfo.textContent = "Please, apply your email address";
+               }
+        
+    })
+    
+    
+    
     
 });
 
